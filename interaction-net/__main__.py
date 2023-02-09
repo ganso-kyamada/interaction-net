80f4ec7820bc186fee87cb8d34cd09d1b58ed3c6
ganso-kyamada <thisismystory.1212@gmail.com>
2023-02-09 14:09:23 +0900

    add lottery_result method

--- interaction-net/__main__.py
+++ interaction-net/__main__.py
@@ -4,16 +4,17 @@ from scrape import Scrape
 from storage import Storage
 
 
-def execute():
+def lottery_apply():
     scrape = Scrape()
     storage = Storage()
     for user in storage.csv("users"):
         scrape.login(user["id"], user["pass"])
+        scrape.apply_menu()
         for ground in storage.csv("grounds"):
-            target_type = "利用目的から"
+            purpose_type = "利用目的から"
             for schedule in storage.csv("schedules"):
-                scrape.menu(
-                    target_type,
+                scrape.purpose_menu(
+                    purpose_type,
                     ground["sports_type"],
                     ground["sports_name"],
                     ground["name"],
@@ -21,11 +22,21 @@ def execute():
                 scrape.calender(schedule["date"], schedule["start"], schedule["end"])
                 if scrape.apply(schedule["people"]) is False:
                     break
-                target_type = "目的から"
+                purpose_type = "目的から"
         scrape.complete()
+        scrape.logout()
         time.sleep(random.randint(1, 10))
     scrape.quit()
 
+def lottery_result():
+    scrape = Scrape()
+    storage = Storage()
+    for user in storage.csv("users"):
+        scrape.login(user["id"], user["pass"])
+        scrape.result()
+        scrape.logout()
+        time.sleep(random.randint(1, 10))
+    scrape.quit()
 
 if __name__ == "__main__":
-    execute()
+    lottery_result()
--- interaction-net/scrape.py
+++ interaction-net/scrape.py
@@ -28,17 +28,20 @@ class Scrape:
         user_element.send_keys(user)
         password_element.send_keys(password)
         password_element.send_keys(Keys.ENTER)
-
         self.user = user
+
+    def apply_menu():
+        print(f"INFO[{self.user}]: ApplyMenu")
         try:
             self.driver.find_element(By.LINK_TEXT, "抽選の申込み").click()
         except NoSuchElementException:
             self.driver.find_element(By.XPATH, "//input[@value='次へ']").click()
             self.driver.find_element(By.LINK_TEXT, "抽選の申込み").click()
 
-    def menu(self, target_type, sports_type, sports_name, ground_name):
-        print(f"INFO[{self.user}]: Menu")
-        self.driver.find_element(By.XPATH, f"//input[@value='{target_type}']").click()
+
+    def purpose_menu(self, purpose_type, sports_type, sports_name, ground_name):
+        print(f"INFO[{self.user}]: PurposeMenu")
+        self.driver.find_element(By.XPATH, f"//input[@value='{purpose_type}']").click()
         self.driver.find_element(By.LINK_TEXT, sports_type).click()
         self.driver.find_element(By.LINK_TEXT, sports_name).click()
         self.driver.find_element(By.XPATH, "//input[@value='申込み']").click()
@@ -71,11 +74,23 @@ class Scrape:
         return self.__alert_accept(3) is False
 
     def complete(self):
+        print(f"INFO[{self.user}]: Complate")
         self.driver.find_element(By.LINK_TEXT, "ホーム").click()
         self.driver.find_element(By.LINK_TEXT, "抽選申込みの確認").click()
         self.__screenshot("complete")
+
+    def result(self):
+        print(f"INFO[{self.user}]: Result")
+        try:
+            self.driver.find_element(By.LINK_TEXT, "確認済の抽選結果").click()
+        except NoSuchElementException:
+            self.driver.find_element(By.XPATH, "//input[@value='次へ']").click()
+            self.driver.find_element(By.LINK_TEXT, "確認済の抽選結果").click()
+        self.__screenshot("result")
+
+    def logout(self):
         self.driver.find_element(By.XPATH, "//input[@value='ログアウト']").click()
-        print(f"INFO[{self.user}]: Finish!")
+        print(f"INFO[{self.user}]: Logout!")
 
     def quit(self):
         self.driver.quit()
